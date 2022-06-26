e5988e23c4a59608aeaf0673c95c2c7feeb572b5
QQQtx <1102214883@qq.com>
2022-06-26 13:07:00 +0800

    test

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qtx
+ * @date 2022/6/26 11:29
+ */
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping("/1")
+    public String test() {
+        return "success";
+    }
+}
